a225dcc2097a209f8ad2792a9283f8daf09e006e
Alex Rabin <alexrabin@me.com>
2023-03-19 11:00:45 -0700

    Changed to get serversideprops

--- pages/book/[bookSlug].tsx
+++ pages/book/[bookSlug].tsx
@@ -1,10 +1,10 @@
 import MainLayout from "@/components/MainLayout";
-import { getAllBooks, getBook } from "@/services/sefariaService";
+import { getBook } from "@/services/sefariaService";
 import { Book } from "@/types/Book";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
-import { GetStaticPaths, GetStaticProps } from "next";
+import { GetServerSideProps } from "next";
 import React from "react";
 import slug from "slug";
 
@@ -26,28 +26,7 @@ const BookPage = ({ book }: Props) => {
 
 export default BookPage;
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  const data = await getAllBooks();
-  if (!data) {
-    throw Error("failed to get texts");
-  }
-  const paths = data.map((a) => {
-    return {
-      params: {
-        bookSlug: slug(a, {
-          lower: false,
-          replacement: "_",
-        }),
-      },
-    };
-  });
-  return {
-    paths: paths ?? [],
-    fallback: false,
-  };
-};
-
-export const getStaticProps: GetStaticProps = async (context) => {
+export const getServerSideProps: GetServerSideProps = async (context) => {
   const { params } = context || {};
   if (!params) {
     throw new Error("params do not exist");
