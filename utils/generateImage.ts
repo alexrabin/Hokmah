c10ded1d761e027a48a920b6038da103152cbea3
Alex Rabin <alexrabin@me.com>
2023-03-22 08:02:41 -0700

    Adjusted the url

--- utils/generateImage.ts
+++ utils/generateImage.ts
@@ -1,8 +1,8 @@
 const generateImage = ({ image, title }: { image?: string; title: string }) => {
-  return `https://www.alexrabin.com/api/og?title=${
-    title.replaceAll(" ", "%20")
-  }` +
-    (image ? `&image=${image}` : "");
+  return (
+    `https://hokmah.vercel.com/api/og?title=${title.replaceAll(" ", "%20")}` +
+    (image ? `&image=${image}` : "")
+  );
 };
 
 export default generateImage;
